ff746230f4c93714eb31abcdb6727f1f3dec3a1b
CarolArevalo <are20461@uvg.edu.gt>
2023-03-01 09:05:27 -0600

    wip minimizacion

--- automaton/minimization.py
+++ automaton/minimization.py
@@ -5,8 +5,7 @@ from utils.state import State
 
 class Minimization:
     def __init__(self):
-        self.partition = Set()
-
+        self.conjuntos = []
 
     def Minimize(self, afd):
         minimized = self.hopcroft(afd)
@@ -17,56 +16,83 @@ class Minimization:
         return minimized
     
     def hopcroft(self, afd):
-        minimized = Automaton()
-        minimized.symbols = afd.symbols
-        minimized.initialState = afd.initialState
-        minimized.finalStates = afd.finalStates
-        minimized.states = afd.states
 
-        self.partition.AddItem(afd.finalStates)
-        self.partition.AddItem(afd.states.Difference(afd.finalStates))
+        automata = Automaton()
+        automata.symbols = afd.symbols
 
-        while True:
-            new_partition = Set()
-            for partition in self.partition.elements:
-                partition = self.split(partition, minimized)
-                new_partition = new_partition.Union(partition)
-            if new_partition.Equals(self.partition):
-                break
-            self.partition = new_partition
+        P = [afd.finalStates, afd.states.Difference(afd.finalStates)]
+        W = [afd.finalStates, afd.states.Difference(afd.finalStates)]
 
-        for partition in self.partition.elements:
-            state = State(partition.elements[0].id, 'normal')
-            minimized.addState(state)
-            if partition.Contains(afd.initialState):
-                minimized.initialState = state
-            if partition.Contains(afd.finalStates):
-                minimized.addFinalState(state)
+        while len(W) != 0:
+            A = W[0]
+            W.remove(A)
 
-        for partition in self.partition.elements:
-            for symbol in minimized.symbols.elements:
-                states = Set()
-                for state in partition.elements:
-                    states = states.Union(afd.move(state, symbol))
-                for new_partition in self.partition.elements:
-                    if states.Equals(new_partition):
-                        for state in partition.elements:
-                            minimized.addTransition(state, new_partition.elements[0], symbol)
-                        break
+            for symbol in automata.symbols.elements:
+                X = Set()
 
-        return minimized
-    
-    def split(self, partition, afd):
-        new_partition = Set()
-        for state in partition.elements:
-            for symbol in afd.symbols.elements:
-                states = afd.move(state, symbol)
-                for new_partition in self.partition.elements:
-                    if states.Equals(new_partition):
-                        new_partition.AddItem(state)
-                        break
-        return new_partition
+                for transition in afd.transitions:
+                    if transition[2] == symbol and transition[1] == A:
+                        set = Set()
+                        set.AddItem(transition[0])
+                        X.AddItem(set)
+
+                for Y in P:
+                    if len(X.Intersection(Y).elements) != 0 and len(Y.Difference(X).elements) != 0:
+                        P.remove(Y)
+                        P.append(X.Intersection(Y))
+                        P.append(Y.Difference(X))
+
+                        if Y in W:
+                            W.remove(Y)
+                            W.append(X.Intersection(Y))
+                            W.append(Y.Difference(X))
+                        else:
+                            if len(X.Intersection(Y).elements) <= len(Y.Difference(X).elements):
+                                W.append(X.Intersection(Y))
+                            else:
+                                W.append(Y.Difference(X))
+
+        self.conjuntos = P
+        self.conjuntos = sorted(self.conjuntos, key=len, reverse=True)
+        # self.conjuntos = sorted(self.conjuntos, key=lambda x: list(x))
 
 
-        
+        counter = 0
+        states_list = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
+
+        for conjunto in self.conjuntos:
+            state = State(states_list[counter], 'normal', conjunto)
+            automata.addState(state)
+            counter += 1
+
+
+        for state in automata.states.elements:
+
+            for inner_state in state.AFN_states.elements:
+                if inner_state.type == 'inicial':
+                    state.type = 'inicial'
+                    automata.initialState = state
+                    break
+                if inner_state.type == 'final':
+                    state.type = 'final'
+                    automata.addFinalState(state)
+                    break
+                if inner_state.type == 'final_inicial':
+                    state.type = 'final_inicial'
+                    automata.addFinalState(state)
+                    automata.initialState = state
+                    break
+
+           
+        for transicion in afd.transitions:
+            automata.addTransition(self.findState(transition[0], automata),self.findState(transicion[1], automata),transicion[2])
+
+        return automata
+
+    def findState(self, state, automata):
+        for state1 in automata.states.elements:
+            for state2 in state1.AFN_states.elements:
+                if state.id == state2.id:
+                    return state1
+        return None
